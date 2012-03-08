eb91fd6b61ed6719a1cd2675100a9dc6a65282e7
Ed Lee <edward.sklee@gmail.com>
2012-03-09 00:13:50 +1100

    add javadoc

--- src/main/java/com/kole/cache/hashing/ConsistentHashing.java
+++ src/main/java/com/kole/cache/hashing/ConsistentHashing.java
@@ -1,9 +1,18 @@
 package com.kole.cache.hashing;
 
+/**
+ * 
+ * @author edwardlee
+ * 
+ */
 public interface ConsistentHashing {
-	void addNode(Node node); 
+	void addNode(Node node);
+
 	void deleteNode(Node node);
+
 	Node findNode(byte[] key);
+
 	int size();
+
 	String hashing(String key);
 }
